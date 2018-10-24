f78e299bbf9760dcea3ffd96386e682d2da06b47
Jenny Poon <jenny.poon@live.ca>
2018-10-24 01:23:37 +0000

    updated classes to use new account object

--- index.js
+++ index.js
@@ -8,24 +8,26 @@ class Account {
 
 class Withdrawal {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 
 }
 
 class Deposit {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
   }
 
 }
@@ -37,18 +39,18 @@ class Deposit {
 const myAccount = new Account("snow-patrol");
 
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
 
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
