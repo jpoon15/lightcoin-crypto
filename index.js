d400bf4ae6ee762a47ea0e2f23503163c0a92c37
Jenny Poon <jenny.poon@live.ca>
2018-10-24 04:25:04 +0000

    used getter method to share commit method

--- index.js
+++ index.js
@@ -10,22 +10,25 @@ class Transaction {
 
   constructor(amount, account) {
     this.amount = amount;
-    this.account = account
+    this.account = account;
+  }
+  //action that deducts from account balance
+  commit() {
+    this.account.balance += this.value;
   }
 }
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
-
 }
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 
 }
@@ -36,18 +39,20 @@ class Deposit extends Transaction {
 
 const myAccount = new Account("snow-patrol");
 
+console.log(myAccount)
 t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('New Balance after T1:', myAccount.balance);
 
 t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', myAccount.balance);
+console.log('New Balance after T2:', myAccount.balance);
 
 t3 = new Deposit(120.00, myAccount);
 t3.commit();
-console.log('Transaction 3:', t3);
+console.log('New Balance after T3:', myAccount.balance);
 
-console.log('Balance:', myAccount.balance);
+
+
+
+
